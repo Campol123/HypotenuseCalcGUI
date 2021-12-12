3cf308ed7b914f7203109a3766b192fe06d6224b
Campol123 <66687172+Campol123@users.noreply.github.com>
2021-12-12 13:33:45 +0000

    Update HypotenuseCalcGUI.py

--- HypotenuseCalcGUI.py
+++ HypotenuseCalcGUI.py
@@ -1,33 +1,32 @@
-import PySimpleGUI as sg
-import math
+import PySimpleGUI as sg # imports PySimpleGUI module
+import math # imports python math module
 
-layout = [[sg.Text("Welcome to Cameron's Hypotenuse calculator")], [sg.Text("Enter 'A' and 'B'")], [sg.In(size=(25, 1), enable_events=True, key="-A-")], [sg.In(size=(25, 1), enable_events=True, key="-B-")], [sg.Button("  NEXT  ")]]
+layout = [[sg.Text("Welcome to Cameron's Hypotenuse calculator")], [sg.Text("Enter 'A' and 'B'")], [sg.In(size=(25, 1), enable_events=True, key="-A-")], [sg.In(size=(25, 1), enable_events=True, key="-B-")], [sg.Button("  NEXT  ")]] # defines primary page layout
 
-# Create the window
-window = sg.Window("Enter Parameters:", layout, margins=(300, 300))
 
-# Create an event loop
+window = sg.Window("Enter Parameters:", layout, margins=(300, 300)) # opens primary window
+
+
 while True:
-    event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    if event == "  NEXT  " or event == sg.WIN_CLOSED:
+    event, values = window.read() # sets the 'event' and 'values' variables to information from the page
+    if event == "  NEXT  " or event == sg.WIN_CLOSED:# ends while loop when user presses 'NEXT' button
         break
-window.close()
-A = values["-A-"]
-B = values["-B-"]
-A=int(A)
-B=int(B)
+window.close()# closes primary window
 
-hyp_len = math.sqrt(A**2 + B**2)
-hyp_len=str(hyp_len)
+A = values["-A-"] # sets 'A' to appropriate value from 'values'
+B = values["-B-"] # sets 'B' to appropriate value from 'values'
+try: # tries to perform hypotenuse calculation
+    A=float(A)
+    B=float(B)
+    hyp_len = math.sqrt(A**2 + B**2)
+    hyp_len=str(hyp_len)
+except: # sets error to display if the 'A' or 'B' values are incorrect
+    hyp_len=("ERROR- The A or B value was incorrect")
 
-layout = [[sg.Text("The Hypotenuse: " + hyp_len)], [sg.Button("  DONE  ")]]
-window = sg.Window("Enter Parameters:", layout, margins=(300, 300))
+layout = [[sg.Text("The Hypotenuse: " + hyp_len)], [sg.Button("  DONE  ")]] # sets layout of secondary page
+window = sg.Window("Hypotenuse: ", layout, margins=(300, 300)) # opens secondary window
 while True:
-    event, values = window.read()
-    # End program if user closes window or
-    # presses the OK button
-    if event == "  DONE  " or event == sg.WIN_CLOSED:
+    event, values = window.read() # sets the 'event' and 'values' variables to information from the page
+    if event == "  DONE  " or event == sg.WIN_CLOSED:# ends while loop when user presses 'DONE' button
         break
-window.close()
+window.close()# closes secondary window
